07c42f4ea767ce857e75abaca6800bb1928bb7de
Murga <juanmurga97@gmail.com>
2024-04-07 15:03:11 +0200

    fixing mobile events with touch-action: none

--- src/components/Range/RangeBullets.tsx
+++ src/components/Range/RangeBullets.tsx
@@ -1,4 +1,4 @@
-import { PointerEventHandler, useRef, useEffect } from "react";
+import { PointerEventHandler, useRef } from "react";
 
 type Props = {
   isDragging: boolean;
@@ -7,46 +7,27 @@ type Props = {
   isFixed: boolean;
 };
 
-export const RangeBullets = ({
-  isDragging,
-  setIsDragging,
-  bulletPosition,
-  isFixed = false,
-}: Props) => {
+export const RangeBullets = ({ isDragging, setIsDragging, bulletPosition, isFixed = false }: Props) => {
   const bulletRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const onMouseDown = (e: PointerEvent) => {
-      bulletRef.current?.setPointerCapture(e.pointerId);
-      setIsDragging(true);
-    };
+  const onMouseDown: PointerEventHandler<HTMLDivElement> = (e) => {
+      bulletRef.current?.setPointerCapture(e.pointerId)
+      setIsDragging(true)
+  };
 
-    const onMouseUp = (e: PointerEvent) => {
-      bulletRef.current?.releasePointerCapture(e.pointerId);
-      setIsDragging(false);
-    };
-
-    const bulletElement = bulletRef.current;
-
-    if (bulletElement) {
-      bulletElement.addEventListener("pointerdown", onMouseDown);
-      bulletElement.addEventListener("pointerup", onMouseUp);
-
-      return () => {
-        bulletElement.removeEventListener("pointerdown", onMouseDown);
-        bulletElement.removeEventListener("pointerup", onMouseUp);
-      };
-    }
-  }, [setIsDragging]);
+  const onMouseUp: PointerEventHandler<HTMLDivElement> = (e) => {
+      bulletRef.current?.releasePointerCapture(e.pointerId)
+      setIsDragging(false)
+  };
 
   return (
-    <div
-      ref={bulletRef}
-      className={`${isDragging ? "cursor-grabbing" : "cursor-grab"} ${
-        isFixed ? "transition-all" : "transition-transform"
-      }
-                            absolute w-5 h-5 bottom-[-8px] border border-white rounded-full bg-black cursor-grab hover:scale-150 shadow-md`}
-      style={{ left: `${bulletPosition}%` }}
-    />
-  );
-};
+      <div
+          ref={bulletRef}
+          onPointerDown={onMouseDown}
+          onPointerUp={onMouseUp}
+          className={`${isDragging ? "cursor-grabbing" : "cursor-grab"} ${isFixed ? 'transition-all' : 'transition-transform'}
+                      absolute w-5 h-5 bottom-[-8px] border border-white rounded-full bg-black cursor-grab hover:scale-150 shadow-md`}
+          style={{ left: `${bulletPosition}%` }}
+      />
+  )
+}
